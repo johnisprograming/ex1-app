bf5dee3fcff7753aa6ff302a07840bc51cfb54ea
johnisprograming <ojh3995@gmail.com>
2022-03-16 11:06:59 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Content from './components/Content';
+import Header from './components/Header';
 
 function App() {
+  // 배열 반환 = useState(초기값);
+  let [number, setNumber] = useState(0);
+
+  // [+] 버튼 클릭했을 호출되는 함수
+  // number 값을 1 증가하는 일 수행
+  const onAdd = () => {
+    // state 값을 변경할 때는 반드시 setter를 사용해야 함 : setNumber()
+    setNumber(number + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App 입니다 : {number} &nbsp;
+      {/* onClick={함수명} */}
+      <button onClick={onAdd}>+</button>
+
+      <Header number={number}></Header>
+      <Content onAdd={onAdd}></Content>
     </div>
   );
 }
